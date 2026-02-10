3b44f4b3709274a231870ad69b0a38a5345fdc35
emmy grammy <ojeabuo24@gmail.com>
2026-02-10 01:47:51 +0100

    first commit

---
+++ src/component/Friends.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && <p className="red">you owe {Math.abs(friend.balance)} $</p>}
+      {friend.balance > 0 && <p className="green">{friend.name} owes you {friend.balance} $</p>}
+      {friend.balance === 0 && <p>you and {friend.name} are even</p>}
+      <button className="button">select</button>
+    </li>
+  );
+}
+
+function FriendsList() {
+  return (
+    <ul>
+      {initialFriends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+export default FriendsList
